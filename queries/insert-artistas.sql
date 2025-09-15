a1375443f252edb80fb9ad48ac3e1b332038296c
GustavoAlvess <gustavo.a.lima21@aluno.senai.br>
2025-09-15 09:56:59 -0300

    feat: preenche tabela

--- queries/insert-artistas.sql
+++ queries/insert-artistas.sql
@@ -9,3 +9,11 @@ CREATE TABLE artistas {
     MUSICA_MAIS_OUVIDA VARCHAR(50),
     ATIVO BOOLEAN DEFAULT TRUE
 };
+
+
+INSERT INTO artistas (NOME,TIPO,MEMBROS, GENERO_MUSICAL, PAIS_ORIGEM, ANO_INICIO, MUSICA_MAIS_OUVIDA, ATIVO)
+VALUES('Thales Roberto', 'Artista Solo', 1, 'Gospel, Soul, Pop', 'Brasil', 2003, 'Deus da Minha Vida', TRUE),
+
+VALUES('Morada', 'Banda', 4, 'Gospel, Rock Alternativo', 'Brasil', 2009, 'É Tudo Sobre Você', TRUE),
+
+VALUES ('Bruno Mars', 'Artista Solo', 1, 'Pop, R&B, Funk, Soul', 'Estados Unidos', 2004, 'Uptown Funk', TRUE);
