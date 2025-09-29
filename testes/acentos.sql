066a68ef47ecd233d656be29ce76dbffff56779f
GustavoAlvess <gustavo.a.lima21@aluno.senai.br>
2025-09-29 11:42:12 -0300

    feat: adiciona acentos.sql

---
+++ testes/acentos.sql
@@ -0,0 +1,18 @@
+CREATE TABLE testes2 (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL
+);
+
+INSERT INTO testes2 (nome)
+VALUES
+('João'),
+('Ângelo'),
+('Érica'),
+('Ótavio'),
+('Lúcia'),
+('Márcia'),
+('Renée'),
+('Cézar'),
+('Fábio'),
+('Bráulio');    
+    
